324a806c4c9554538cfb1a4236b52de1e1a711e4
santoshkumar <skumarchougala@gmail.com>
2025-04-23 19:45:09 +0530

    "commit"

--- src/WritingFileInBufferAndPrintWriter.java
+++ src/WritingFileInBufferAndPrintWriter.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class WritingFileInBufferAndPrintWriter {
+    public static void main(String[] args) {
+        File file = new File("Heaven.txt");
+        try {
+            //printWriter provides methods like print and println and printf
+            //BufferWriter provides boost the performance and it will store text not charcter wise
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
+            writer.println("hey java developer");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/WritingToFile.java
+++ src/WritingToFile.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WritingToFile {
+    public static void main(String[] args) {
+        try{
+            File file = new File("Heaven.txt");
+            FileWriter writer = new FileWriter(file,true);
+            //If you want to add content without erasing old text,(file,true) append the true
+            writer.write("Hello Santosh! ✨\nWelcome to Java File Handling.\nLet's master it step by step.");
+
+            writer.close(); // Save content properly
+            // Without closing, the content stays in memory and might not reach the file.
+
+            System.out.println("In file Writed");
+        } catch (IOException e) {
+            System.out.println("❌ An error occurred while writing.");
+            e.printStackTrace();
+        }
+    }
 }
